cdca9caf6429f21de3aa15191132d11960b17912
ARGFL <165548488+ARGFL@users.noreply.github.com>
2024-06-29 15:39:38 +0300

    Update fruits slides

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-const words = ["apple", "banana", "strawberries"];
+const words = ["apple", "banana", "pear","plum","grapes","cherry","berry","strawberry","blueberry","raspberry","orane","mango","melon","watermelon","pineapple"];
 
 const correctPronunciation = {
     "apple": "./recordingfruit/apple.mp3",
     "banana": "./recordingfruit/banana.mp3",
-    "strawberries": "./recordingfruit/strawberries.mp3"
+    "pear":"./recordingfruit/pear.mp3",
+    "plum":"./recordingfruit/plum .mp3",
+    "grapes":"./recordingfruit/grapes.mp3",
+    "cherry":"./recordingfruit/cherry.mp3",
+    "berry":"./recordingfruit/berry.mp3",
+    "strawberry": "./recordingfruit/strawberry.mp3",
+    "blueberry":"./recordingfruit/blueberry.mp3",
+    "raspberry":"./recordingfruit/raspberry.mp3",
+    "orange":"./recordingfruit/orange.mp3",
+    "mango":"./recordingfruit/mango.mp3",
+    "melon":"./recordingfruit/pepene galben.mp3",
+    "watermelon":"./recordingfruit/pepene rosu.mp3",
+    "pineapple":"./recordingfruit/pineapple.mp3",
+   
 };
 
 const retryAudio = "./retry.mp3"; // Audio file for retry message
@@ -11,13 +24,38 @@ const retryAudio = "./retry.mp3"; // Audio file for retry message
 const fruitImages = {
     "apple": "./images/apple.png",
     "banana": "./images/banana.png",
-    "strawberries": "./images/strawberry.png"
+    "pear":"./images/pear.jpg",
+    "plum":"./images/plum.jpg",
+    "grapes":"./images/grapes.jpg",
+    "cherry":"./images/cherry.jpg",
+    "berry":"./images/berry.jpg",
+    "strawberry": "./images/strawberry.png",
+    "blueberry":"./images/blueberry.jpg",
+    "raspberry":"./images/raspberry.jpg",
+    "orange":"./images/orange.jpg",
+    "mango":"./images/mango.jpg",
+    "melon":"./images/melon.jpg",
+    "watermelon":"./images/watermelon.jpg",
+    "pineapple":"./images/pineapple.jpg",
+    
 };
 
 const translations = {
     "apple": "măr",
     "banana": "banană",
-    "strawberries": "căpsuni"
+     "pear":"pară",
+    "plum":"prună",
+    "grapes":"struguri",
+    "cherry":"cireașă",
+    "berry":"fruct de pădure",
+    "strawberries": "căpșuni",
+    "blueberry":"afine",
+    "raspberry":"zmeură",
+    "orange":"portocală",
+    "mango":"mango",
+    "melon":"pepene",
+    "watermelon":"pepene roșu",
+    "pineapple":"ananas",
 };
 
 let currentWordIndex = 0;
@@ -147,11 +185,11 @@ function checkWriting() {
     const nextWordButton = document.getElementById("nextWordButton");
 
     if (userInput === correctWord) {
-        feedbackElement.textContent = "Correct! Well done.";
+        feedbackElement.textContent = "Corect, bravo!";
         feedbackElement.style.color = "green";
         nextWordButton.disabled = false;
     } else {
-        feedbackElement.textContent = "Incorrect, please try again.";
+        feedbackElement.textContent = "Incorect, mai  încearcă odată.";
         feedbackElement.style.color = "red";
         nextWordButton.disabled = true;
     }
